fdaeda171036b0a7f373e966677d8e0e6475ec65
Eduard <eduard@admintools.io>
2024-11-28 11:36:16 +0200

    Notifications

--- src/app/(dashboard)/notifications/page.tsx
+++ src/app/(dashboard)/notifications/page.tsx
@@ -21,12 +21,12 @@ export default function Notifications() {
   return (
     <div className="flex flex-col items-center gap-4 justify-center bg-blue-50">
       <div className="flex items-center gap-4 p-2 w-[600px] justify-between">
-        <div className="flex gap-4">
+        <div className="flex gap-4 py-4">
           <div className="relative w-12 h-12">
-            <div className="absolute inset-0 flex justify-center items-center rounded-full bg-blue-600 w-10 h-10">
+            <div className="absolute inset-0 top-2 flex justify-center items-center rounded-full bg-blue-600 w-10 h-10">
               <span className="text-white text-xl font-bold">-</span>
             </div>
-            <div className="absolute inset-0 left-4 top-2 flex justify-center items-center rounded-full bg-green-500 w-10 h-10">
+            <div className="absolute inset-0 left-4 top-4 flex justify-center items-center rounded-full bg-green-500 w-10 h-10">
               <span className="text-white text-xl font-bold">+</span>
             </div>
           </div>
