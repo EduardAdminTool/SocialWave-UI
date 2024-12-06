662b13a7d68cdbfc1d954cfbf01b96c68c09e24a
Eduard <eduard@admintools.io>
2024-12-06 11:52:29 +0200

    delete post

--- src/components/PostModal.tsx
+++ src/components/PostModal.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import React, { useState } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Heart, MessageCircle, Send, Bookmark } from "lucide-react";
 import Image from "next/image";
 import { Post } from "@/types/posts/types";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import { MdDelete } from "react-icons/md";
+import { deletePost } from "@/services/posts";
 interface PostModalProps {
   post: Post | null;
   isOpen: boolean;
@@ -17,8 +24,15 @@ interface PostModalProps {
 export function PostModal({ post, isOpen, onClose }: PostModalProps) {
   const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
 
+  const deletePostButton = async () => {
+    await deletePost(Number(post?.postId));
+  };
+
   const nextMedia = () => {
-    if (post && post.images.length + post.videos.length > currentMediaIndex + 1) {
+    if (
+      post &&
+      post.images.length + post.videos.length > currentMediaIndex + 1
+    ) {
       setCurrentMediaIndex(currentMediaIndex + 1);
     }
   };
@@ -57,9 +71,7 @@ export function PostModal({ post, isOpen, onClose }: PostModalProps) {
         <div className="flex flex-col md:flex-row">
           <div className="w-full md:w-2/3 relative h-[400px] md:h-[600px]">
             {(post.images.length > 0 || post.videos.length > 0) && (
-              <div
-                className="relative flex h-full justify-center items-center overflow-hidden"
-              >
+              <div className="relative flex h-full justify-center items-center overflow-hidden">
                 {currentMediaIndex > 0 && (
                   <Button
                     variant="ghost"
@@ -80,7 +92,8 @@ export function PostModal({ post, isOpen, onClose }: PostModalProps) {
                     transition={{ type: "spring", stiffness: 300, damping: 30 }}
                     className="absolute inset-0 flex justify-center items-center"
                   >
-                    {post.images.length > 0 && currentMediaIndex < post.images.length ? (
+                    {post.images.length > 0 &&
+                    currentMediaIndex < post.images.length ? (
                       <Image
                         src={post.images[currentMediaIndex].imageUrl}
                         alt={`Post image ${currentMediaIndex + 1}`}
@@ -96,15 +109,27 @@ export function PostModal({ post, isOpen, onClose }: PostModalProps) {
                           onError={() => console.log("Video failed to load")}
                         >
                           <source
-                            src={post.videos[currentMediaIndex - post.images.length].videoUrl}
+                            src={
+                              post.videos[
+                                currentMediaIndex - post.images.length
+                              ].videoUrl
+                            }
                             type="video/mp4"
                           />
                           <source
-                            src={post.videos[currentMediaIndex - post.images.length].videoUrl}
+                            src={
+                              post.videos[
+                                currentMediaIndex - post.images.length
+                              ].videoUrl
+                            }
                             type="video/webm"
                           />
                           <source
-                            src={post.videos[currentMediaIndex - post.images.length].videoUrl}
+                            src={
+                              post.videos[
+                                currentMediaIndex - post.images.length
+                              ].videoUrl
+                            }
                             type="video/ogg"
                           />
                           Your browser does not support the video tag.
@@ -114,7 +139,8 @@ export function PostModal({ post, isOpen, onClose }: PostModalProps) {
                   </motion.div>
                 </AnimatePresence>
 
-                {currentMediaIndex < post.images.length + post.videos.length - 1 && (
+                {currentMediaIndex <
+                  post.images.length + post.videos.length - 1 && (
                   <Button
                     variant="ghost"
                     size="icon"
@@ -141,7 +167,7 @@ export function PostModal({ post, isOpen, onClose }: PostModalProps) {
                   <Button variant="ghost">
                     <Heart className="h-6 w-6" />
                   </Button>
-                  <Button variant="ghost" >
+                  <Button variant="ghost">
                     <MessageCircle className="h-6 w-6" />
                   </Button>
                   <Button variant="ghost">
@@ -150,6 +176,9 @@ export function PostModal({ post, isOpen, onClose }: PostModalProps) {
                 </div>
                 <Button variant="ghost">
                   <Bookmark className="h-6 w-6" />
+                </Button>
+                <Button variant="ghost" onClick={deletePostButton}>
+                  <MdDelete className="h-6 w-6" />
                 </Button>
               </div>
               <p className="font-bold">150mock likes</p>
